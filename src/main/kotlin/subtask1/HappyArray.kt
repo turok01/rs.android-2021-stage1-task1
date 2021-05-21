5c2e489e7b8462fb9c3d35bd48017c1776e4a2a8
turok01 <toolbox@tut.by>
2021-05-21 23:41:56 +0300

    feat: solve task BillCounter and task HappyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,26 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        var happy: Boolean = false
+        var arraySize: Int = sadArray.size
+        while(!happy) {
+            happy = true
+            for (i in 0..arraySize - 1) {
+                if (i > 0 && i < arraySize - 1) { //dont check the extreme(border) elements
+                    if (sadArray[i] > sadArray[i - 1] + sadArray[i + 1]) { //if not happy
+                        happy = false
+                        for (j in i..arraySize - 2) {// shift the elements of array
+                            sadArray[j] = sadArray[j + 1]
+                        }
+                        arraySize-- // size of new happy array will by less
+                    }
+                }
+            }
+        }
+        var happyArray: IntArray = intArrayOf()
+        for (i in 0..arraySize-1)
+            happyArray += sadArray[i]
+        return happyArray
     }
 }
--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,16 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        var billAmount: Int = 0;
+        for (i in 0..bill.size - 1) {
+            billAmount+=bill[i];
+        }
+        billAmount -= bill[k];
+        var difference: Int = b - billAmount/2
+        if (difference == 0)
+            return "Bon Appetit"
+        else
+            return difference.toString()
     }
 }
