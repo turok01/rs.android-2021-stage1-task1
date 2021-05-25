f18b34196497ff114e0a11b9c9caa345c814d3ba
Asus-PC <toolbox@tut.by>
2021-05-25 23:31:20 +0300

    feat: start StringParser task

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,6 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
-    }
+        //throw NotImplementedError("Not implemented")
+        }
 }
